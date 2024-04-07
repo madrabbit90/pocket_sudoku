ddc66cf109568fa015d0ba2a67e59b02ad2afa1a
madrabbit90 <134638980+madrabbit90@users.noreply.github.com>
2024-04-06 23:36:11 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,4 +1,3 @@
-GNU nano 7.2                                                                                                      cues.cpp
 #include <iostream>
 #include <ncurses.h>
 #include <string>
