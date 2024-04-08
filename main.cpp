86687f8642ce8215adba902c3622e3aa29c7eaa0
madrabbit90 <134638980+madrabbit90@users.noreply.github.com>
2024-04-07 20:26:32 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -4,7 +4,7 @@
 
 using namespace std;
 
-char initpuzz[] = "070000043040009610800634900094052000358460020000800530080070091902100005007040802";
+#include "puzzles.h"
 
 int xpos = 0;
 int ypos = 0;
@@ -17,87 +17,10 @@ bool givens[9][9];
 
 string s = "123456789";
 
-void puzzinit()
-{
-    for (int i; i<81; i++)
-    {
-        pfield[i%9][i/9]=initpuzz[i];
-        if(s.find(initpuzz[i])<s.length())
-                givens[i%9][i/9]=true;
-        else
-                givens[i%9][i/9]=false;
-    }
-}
-
-bool rules()
-{
-    for (int i=0; i<9; i++)
-    {
-        if (pfield[i][ypos] == padder)
-            return false;
-        if (pfield[xpos][i] == padder)
-            return false;
-        if (pfield[((xpos/3)*3)+i%3][((ypos/3)*3)+i/3] == padder)
-            return false;
-    }
-    return true;
-}
-
-void redraw() {
-    move(1, 0);
-    printw("POCKET SuDoKu");
-    for (int i = 0; i<403; i++)
-    {
-         move(i%13+yoff, i/13+xoff);
-         if ((((i%13)%4)==0) && (((i/13)%10)==0)){addch('+');}
-         else {if (((i%13)%4)==0){addch('-');}
-         if (((i/13)%10)==0){addch('|');}}
-     }
-    for (int o = 0; o<9; o++)
-    {
-        for (int v = 0; v<9; v++)
-        {
-            if(s.find(pfield[o][v])<s.length())
-            {
-                move(v+yoff+1+v/3, o*3+2+o/3+xoff);
-                addch(pfield[o][v]);
-            }
-            if(givens[o][v])
-            {
-                move(v+yoff+1+v/3, o*3+1+o/3+xoff);
-                addch('(');
-                move(v+yoff+1+v/3, o*3+3+o/3+xoff);
-                addch(')');
-            }
-        }
-    }
-
-}
-void controls() {
-    switch (padder)
-    {
-        case 'k': ypos=ypos-1;
-            break;
-        case 'j': ypos=ypos+1;
-            break;
-        case 'h': xpos=xpos-1;
-            break;
-        case 'l': xpos=xpos+1;
-            break;
-    }
-    if(s.find(padder)<s.length() && givens[xpos][ypos] == false && rules())
-    {
-        pfield[xpos][ypos] = padder;
-    }
-    if (ypos > 8)
-        ypos = 8;
-    if (ypos < 0)
-        ypos = 0;
-    if (xpos > 8)
-        xpos = 8;
-    if (xpos < 0)
-        xpos = 0;
-}
+#include "init.h"
+#include "logic.h"
+#include "draw.h"
+#include "controls.h"
 
 int main(int argc, char ** argv)
 {
